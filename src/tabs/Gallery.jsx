11129383084c0fee4b99e473a082058b03398eb2
Oleg Adamov <103196379+OlegAdamov@users.noreply.github.com>
2022-10-30 17:28:41 +0200

    Update Gallery.jsx

--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -1,7 +1,7 @@
 import { Component } from 'react';
 
-import * as ImageService from 'service/image-service';
-import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
+// import * as ImageService from 'service/image-service';
+// import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
 
 export class Gallery extends Component {
   render() {
