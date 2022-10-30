f2d77abb9ae0a765046bca5ce035c08e142b4b70
Oleg Adamov <103196379+OlegAdamov@users.noreply.github.com>
2022-10-30 17:24:31 +0200

    Update Todo.jsx

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,6 +1,12 @@
 import { Text } from 'components';
-import { TodoWrapper, DeleteButton, EditButton } from './Todo.styled';
-import { RiDeleteBinLine, RiEdit2Line } from 'react-icons/ri';
+import { TodoWrapper,
+        DeleteButton, 
+// EditButton 
+       } from './Todo.styled';
+import { 
+  RiDeleteBinLine, 
+// RiEdit2Line 
+} from 'react-icons/ri';
 
 export const Todo = ({ dataTodo, idx }) => {
   return (
